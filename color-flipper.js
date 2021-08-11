ff4eebc89c466e0efed97f1191d5e9c602ff742d
Samuel Nyamekesse <snyamekesse@stu.ucc.edu.gh>
2021-08-11 23:50:51 +0100

    made changes to the color-flipper.js by adding functionalities

--- color-flipper.js
+++ color-flipper.js
@@ -1,2 +1,16 @@
-const allColors = ["red", "green", "pink", "ash", "yellow", "blue", "orange"];
-const btn = document.querySelector('btnColor');
+const allColors = ["white", "red", "green", "pink", "violet", "yellow", "blue", "orange", "grey"];
+const btn = document.querySelector('#btnColor');
+const showCol = document.querySelector('.colorShow');
+const textCol = document.querySelector('main .show h1');
+
+btn.addEventListener('click', ()=> {
+    let randomNum = getRandomNumber();
+    document.body.style.backgroundColor = allColors[randomNum];
+    showCol.textContent = allColors[randomNum];
+    textCol.style.color = allColors[randomNum];
+    showCol.style.color = allColors[randomNum];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * allColors.length);
+}
