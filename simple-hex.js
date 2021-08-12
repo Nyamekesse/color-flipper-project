8d9e8cae89ba71d9b05eb10197f118ef422db165
Samuel Nyamekesse <snyamekesse@stu.ucc.edu.gh>
2021-08-12 22:46:14 +0100

    added functionality to simple-hex.js

--- simple-hex.js
+++ simple-hex.js
@@ -1,4 +1,27 @@
 const items = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const btn = document.querySelector('#btnColor');
 const showColor = document.querySelector('.colorShow');
+const textColor = document.querySelector('main .show h1');
+const topText = document.querySelectorAll('#topMenu h2 a');
+console.log(topText);
 
+btn.addEventListener('click', ()=> {
+    let colorHex = "#";
+    topText.forEach((item) => {
+        item.style.color = 'white';
+    });
+
+    for (let i=0; i<6; i++){
+        colorHex += items[getRandomNum()]
+    }
+
+    document.body.style.backgroundColor = colorHex;
+    textColor.style.color = colorHex;
+    showColor.textContent = colorHex;
+    showColor.style.color = colorHex;
+
+})
+
+function getRandomNum(){
+    return Math.floor(Math.random() * items.length);
+}
